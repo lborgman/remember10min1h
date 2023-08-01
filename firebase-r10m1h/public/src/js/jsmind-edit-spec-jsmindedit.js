0a04fea09f3cffe08c434ebb06d01bba628a9d4d
lborgman <lennart.borgman@gmail.com>
2023-08-01 04:00:53 +0200

    Working on jsmind-edit-gh.html

--- firebase-r10m1h/public/src/js/jsmind-edit-spec-jsmindedit.js
+++ firebase-r10m1h/public/src/js/jsmind-edit-spec-jsmindedit.js
@@ -1,8 +1,13 @@
 console.log("Here is jsmind-edit-spec-jsmindedit.js");
 
 // const modDb = await import("/src/js/db-fc4i.js");
-const modDb = await import("/src/js/db-mindmaps.js");
-const modJsEditCommon = await import("/src/js/jsmind-edit-common.js");
+
+// const modDb = await import("/src/js/db-mindmaps.js");
+const modDb = await import("db-mindmaps");
+
+// const modJsEditCommon = await import("/src/js/jsmind-edit-common.js");
+const modJsEditCommon = await import("jsmind-edit-common");
+
 modDb.setDBprefix("jsmindedit-");
 
 export function dialogMindMapsJsmindEdit(info, arrMindmapsHits) {
