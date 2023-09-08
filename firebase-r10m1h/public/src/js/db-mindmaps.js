b0fb1519830ed39ab3d842ceeab308f0cdc74c87
lborgman <lennart.borgman@gmail.com>
2023-09-08 14:01:40 +0200

    Prepare db-mindmaps.js for idb

--- firebase-r10m1h/public/src/js/db-mindmaps.js
+++ firebase-r10m1h/public/src/js/db-mindmaps.js
@@ -1,5 +1,11 @@
 "use strict";
 
+console.log("here is module db-mindmaps.js", import.meta);
+if (document.currentScript) throw Error("import .currentScript"); // is module
+if (!import.meta.url) throw Error("!import.meta.url"); // is module
+
+const useLocalStorage = true;
+
 let strPrefix = "jsmindmap-";
 export function setDBprefix(prefix) {
     strPrefix = prefix;
@@ -8,37 +14,53 @@ export function setDBprefix(prefix) {
 
 export async function DBgetAllMindmaps() {
     const all = [];
-    for (let lsKey in localStorage) {
-        if (lsKey.startsWith(strPrefix)) {
-            const json = localStorage[lsKey];
-            const jsmindmap = JSON.parse(json);
-            const key = lsKey.substring(strPrefix.length);
-            const rec = { key, jsmindmap }
-            all.push(rec);
+    if (useLocalStorage) {
+        for (let lsKey in localStorage) {
+            if (lsKey.startsWith(strPrefix)) {
+                const json = localStorage[lsKey];
+                const jsmindmap = JSON.parse(json);
+                const key = lsKey.substring(strPrefix.length);
+                const rec = { key, jsmindmap }
+                all.push(rec);
+            }
         }
+    } else {
+        throw Error("NIY");
     }
     return all;
 }
 export async function DBsetMindmap(keyName, objMindmap) {
     if (keyName !== objMindmap.meta.name) throw Error(`key=${keyName} but objMindmap.meta.name=${objMindmap.meta.name}`);
-    const lsKey = strPrefix + keyName;
-    const json = JSON.stringify(objMindmap);
-    localStorage.setItem(lsKey, json);
+    if (useLocalStorage) {
+        const lsKey = strPrefix + keyName;
+        const json = JSON.stringify(objMindmap);
+        localStorage.setItem(lsKey, json);
+    } else {
+        throw Error("NIY");
+    }
 }
 export async function DBgetMindmap(key) {
     const lsKey = strPrefix + key;
     const json = localStorage.getItem(lsKey);
     const obj = JSON.parse(json);
     if (key !== obj.meta.name) {
-        console.error(`key=${key} but obj.meta.key=${obj.meta.key}`);
-        alert("Old format, fixing");
-        obj.meta.name = key;
-        delete obj.meta.key;
-        DBsetMindmap(key, obj);
+        if (useLocalStorage) {
+            console.error(`key=${key} but obj.meta.key=${obj.meta.key}`);
+            alert("Old format, fixing");
+            obj.meta.name = key;
+            delete obj.meta.key;
+            DBsetMindmap(key, obj);
+        } else {
+            throw Error("NIY");
+        }
     }
     return obj;
 }
 export async function DBremoveMindmap(key) {
-    const lsKey = strPrefix + key;
-    localStorage.removeItem(lsKey);
+    if (useLocalStorage) {
+        const lsKey = strPrefix + key;
+        localStorage.removeItem(lsKey);
+    } else {
+        throw Error("NIY");
+    }
 }
