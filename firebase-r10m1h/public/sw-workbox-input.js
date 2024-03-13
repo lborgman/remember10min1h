a2dd5346025688a07b23f71cc5ee9cb97b9b4138
lborgman <lennart.borgman@gmail.com>
2024-03-13 14:03:27 +0100

    Prepare to add xmpdata to image blob

--- firebase-r10m1h/public/sw-workbox-input.js
+++ firebase-r10m1h/public/sw-workbox-input.js
@@ -1,5 +1,5 @@
 //========== Specific ====================================================
-const SW_VERSION = "0.4.562";
+const SW_VERSION = "0.4.565";
 // throw Error("Test worker error");
 const logColors = "color: green; background: yellow;";
 console.log(`%csw-worker-input.js ${SW_VERSION} is here`, logColors + " font-size: 20px;");
