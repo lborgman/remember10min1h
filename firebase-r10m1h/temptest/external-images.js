e243d5b68f21322d1822f782090dc9c24bbd9581
lborgman <lennart.borgman@gmail.com>
2024-04-05 01:38:47 +0200

    More work on test-moving-lines UI Also tried making mm4i work offline. Failed.

---
+++ firebase-r10m1h/temptest/external-images.js
@@ -0,0 +1,5 @@
+// Module for linking external images.
+// The user provides the links which I guess will avoid copyright problems.
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image
+console.log("here is external-images.js");
